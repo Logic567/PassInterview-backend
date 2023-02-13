815a2ed42944e254bfbb79f82cf6407b0145bf66
Logic <457547174@qq.com>
2023-02-13 13:36:29 +0800

    创建网关模块，使用nacos注册中心，将网关、question、renren-fast注册进nacos中

--- passinterview-question/src/main/java/com/logic/passinterview/question/PassinterviewQuestionApplication.java
+++ passinterview-question/src/main/java/com/logic/passinterview/question/PassinterviewQuestionApplication.java
@@ -3,9 +3,11 @@ package com.logic.passinterview.question;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com/logic/passinterview/question/dao")
+@EnableDiscoveryClient
 public class PassinterviewQuestionApplication {
 
     public static void main(String[] args) {
