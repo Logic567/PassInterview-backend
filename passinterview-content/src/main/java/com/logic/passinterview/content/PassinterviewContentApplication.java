e89233f79a040d7550cc51451e4767068ea336e1
Logic <457547174@qq.com>
2023-02-14 00:15:02 +0800

    五个微服务模块注册进nacos

--- passinterview-content/src/main/java/com/logic/passinterview/content/PassinterviewContentApplication.java
+++ passinterview-content/src/main/java/com/logic/passinterview/content/PassinterviewContentApplication.java
@@ -3,9 +3,11 @@ package com.logic.passinterview.content;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com/logic/passinterview/content/dao")
+@EnableDiscoveryClient
 public class PassinterviewContentApplication {
 
     public static void main(String[] args) {
--- passinterview-study/src/main/java/com/logic/passinterview/study/PassinterviewStudyApplication.java
+++ passinterview-study/src/main/java/com/logic/passinterview/study/PassinterviewStudyApplication.java
@@ -3,9 +3,11 @@ package com.logic.passinterview.study;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com/logic/passinterview/study/dao")
+@EnableDiscoveryClient
 public class PassinterviewStudyApplication {
 
     public static void main(String[] args) {
